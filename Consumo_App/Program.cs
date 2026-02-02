2c5f380f21dcaf15b00d949b5811447e9eb44b50
Enmanuel Ovalles <e.ovalles@outlook.com>
2026-02-01 23:00:42 -0400

    cambio en el connection string al de smarterasp

--- Consumo_App/Program.cs
+++ Consumo_App/Program.cs
@@ -148,7 +148,7 @@ var app = builder.Build();
 // =======================
 // PIPELINE
 // =======================
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
